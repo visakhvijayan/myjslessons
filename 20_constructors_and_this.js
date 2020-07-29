650cc23417b7634ab52cd4276b0c3f5709efbe93
visakh <mailtovisakhv@gmail.com>
2020-07-29 21:05:45 +0530

    added constructor and this file

--- 20_constructors_and_this.js
+++ 20_constructors_and_this.js
@@ -13,13 +13,13 @@ function Person(fname, lname, dob) {
 }
 
 /*
-# In JavaScript, a constructor gets called when you declare an object using the new keyword.
-# The purpose of a constructor is to create an object and set values if there are any object properties present. 
-# It’s a neat way to create an object because you do not need to explicitly state what to return as the constructor function, by default, returns the object that gets created within it.
- # A new empty object is created
- # this keyword starts referring to that newly created object and hence it becomes the current instance object
- # The newly created object is then returned as the constructor’s returned value
- */
+  # In JavaScript, a constructor gets called when you declare an object using the new keyword.
+  # The purpose of a constructor is to create an object and set values if there are any object properties present. 
+  # It’s a neat way to create an object because you do not need to explicitly state what to return as the constructor function, by default, returns the object that gets created within it.
+   # A new empty object is created
+   # this keyword starts referring to that newly created object and hence it becomes the current instance object
+   # The newly created object is then returned as the constructor’s returned value
+   */
 const visakh = new Person("Visakh", "Vijayan", "3-Jun-1991");
 const kavya = new Person("Kavya", "Vinod", "25-Aug-1993");
 
